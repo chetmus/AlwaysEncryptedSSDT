55809234120d3d1140d33a590025c72bd6f80763
chetmus <chetmus@gmai.com>
2017-04-28 15:03:19 -0400

    initial

---
+++ Authentication/Tables/AspNetUserClaims.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Authentication].[AspNetUserClaims] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]     NVARCHAR (128) NOT NULL,
+    [ClaimType]  NVARCHAR (MAX) NULL,
+    [ClaimValue] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Authentication.AspNetUserClaims] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Authentication.AspNetUserClaims_Authentication.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [Authentication].[AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [Authentication].[AspNetUserClaims]([UserId] ASC);
+
---
+++ Authentication/Tables/AspNetUserLogins.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [Authentication].[AspNetUserLogins] (
+    [LoginProvider] NVARCHAR (128) NOT NULL,
+    [ProviderKey]   NVARCHAR (128) NOT NULL,
+    [UserId]        NVARCHAR (128) NOT NULL,
+    CONSTRAINT [PK_Authentication.AspNetUserLogins] PRIMARY KEY CLUSTERED ([LoginProvider] ASC, [ProviderKey] ASC, [UserId] ASC),
+    CONSTRAINT [FK_Authentication.AspNetUserLogins_Authentication.AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [Authentication].[AspNetUsers] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_UserId]
+    ON [Authentication].[AspNetUserLogins]([UserId] ASC);
+
